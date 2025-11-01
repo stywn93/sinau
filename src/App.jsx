7d526e8de92b2d0a04913ac1927e05c670e01ef4
Iwan Bagus S <stywn93@gmail.com>
2025-11-02 03:42:10 +0700

    change background

--- src/App.jsx
+++ src/App.jsx
@@ -6,18 +6,90 @@ function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <div className="flex justify-center items-center w-screen h-screen">
-      <div className="border border-slate-100 rounded-2xl p-4">
-        <div className="flex justify-center">
-          <img
-            src="./src/assets/react.svg"
-            alt="logo LMS"
-            className="w-23"
-          />
+    <div>
+      <div className="flex p-2 m-3 justify-center items-center min-h-screen">
+        <div className="border p-5 border-slate-200 dark:border-slate-800 rounded-4xl bg-slate-100 dark:bg-slate-900">
+          <div className="flex justify-center mb-2">
+            <div class="block bg-slate-100 shadow-xl p-4 rounded-2xl">
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="36"
+                height="36"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="black"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                class="lucide lucide-log-in"
+                aria-hidden="true"
+              >
+                <path d="m10 17 5-5-5-5"></path>
+                <path d="M15 12H3"></path>
+                <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4"></path>
+              </svg>
+            </div>
+          </div>
+          <div className="m-2">
+            <h1 className="text-3xl font-semibold text-slate-8¥900 dark:text-slate-200 mb-1 text-center">Login to start</h1>
+            <p className="text-slate-900 dark:text-slate-600 text-center text-sm">Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+          <form class="max-w-md mx-auto">
+            <div class="mb-5">
+              <label
+                for="email"
+                class="block mb-2 text-md font-medium text-gray-900 dark:text-white"
+              >
+                Your email
+              </label>
+              <input
+                type="email"
+                id="email"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="user@demo.app"
+                required
+              />
+            </div>
+            <div class="mb-5">
+              <label
+                for="password"
+                class="block mb-2 text-md font-medium text-gray-900 dark:text-white"
+              >
+                Your password
+              </label>
+              <input
+                type="password"
+                id="password"
+                class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                placeholder="password"
+                required
+              />
+            </div>
+            <div class="flex items-start mb-5">
+              <div class="flex items-center h-5">
+                <input
+                  id="remember"
+                  type="checkbox"
+                  value=""
+                  class="w-4 h-4 border border-gray-300 rounded-sm bg-gray-50 focus:ring-3 focus:ring-blue-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-blue-600 dark:ring-offset-gray-800 dark:focus:ring-offset-gray-800"
+                  required
+                />
+              </div>
+              <label
+                for="remember"
+                class="ms-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+              >
+                Remember me
+              </label>
+            </div>
+            <button
+              type="submit"
+              class="text-slate-100 dark:text-slate-900 bg-slate-900 hover:bg-slate-800 focus:ring-4 focus:outline-none focus:ring-slate-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-slate-100 dark:hover:bg-slate-50 dark:focus:ring-slate-800"
+            >
+              Submit
+            </button>
+          </form>
         </div>
-
-        <h1 className="text-center text-4xl py-4 font-bold">Silahkan Login</h1>
-        <input type="text" className="rounded-xl bg-white text-3xl"/>
       </div>
     </div>
   );
