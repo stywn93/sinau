bc913ec008884ecaae464fe3a486b84b855d7ead
Iwan Bagus S <stywn93@gmail.com>
2025-11-06 06:59:28 +0700

    navigation link hover styling

--- src/component/Sidebar.jsx
+++ src/component/Sidebar.jsx
@@ -1,19 +1,31 @@
 import { Link } from "react-router";
 
-export default function Sidebar(){
-  return(
-    <aside className="w-64 flex gap-2 bg-slate-300 flex-col ease-in">
-      <div className="flex justify-center uppercase text-xl bg-slate-500 text-slate-50 w-full" id="sidebarHeader">
+export default function Sidebar() {
+  return (
+    <aside className="w-64 flex gap-4 bg-slate-800 flex-col ease-in p-4 rounded-2xl m-4">
+      <div
+        className="flex justify-center items-center uppercase text-2xl text-slate-50 w-full min-h-[100px]"
+        id="sidebarHeader"
+      >
         <span>Sinau Apps</span>
       </div>
-      <div className="bg-slate-200 text-slate-600 w-full" id="navigation">
-        <ul className="flex flex-col gap-2 list-none">
-          <li className="text-md hover:bg-slate-700"><Link to={"/dashboard"}>Dashboard</Link></li>
-          <li className="text-md hover:bg-slate-700"><Link to={"/dashboard"}>Dashboard</Link></li>
-          <li className="text-md hover:bg-slate-700"><Link to={"/dashboard"}>Dashboard</Link></li>
-          <li className="text-md hover:bg-slate-700"><Link to={"/dashboard"}>Dashboard</Link></li>
+      <hr className="text-slate-50" />
+      <div className="text-slate-50 w-full" id="navigation">
+        <ul className="flex flex-col gap-4 my-4 list-none">
+          <li className="text-md p-4 hover:bg-slate-100 hover:text-slate-800 hover:rounded-2xl">
+            <Link to={"/dashboard"}>Dashboard</Link>
+          </li>
+          <li className="text-md p-4 hover:bg-slate-100 hover:text-slate-800 rounded-2xl">
+            <Link to={"/dashboard"}>Dashboard</Link>
+          </li>
+          <li className="text-md p-4 hover:bg-slate-100 hover:text-slate-800 rounded-2xl">
+            <Link to={"/dashboard"}>Dashboard</Link>
+          </li>
+          <li className="text-md p-4 hover:bg-slate-100 hover:text-slate-800 rounded-2xl">
+            <Link to={"/dashboard"}>Dashboard</Link>
+          </li>
         </ul>
       </div>
     </aside>
-  )
+  );
 }
